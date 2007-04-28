051f797fc086c3483984ec7b4f3394b93352f1cd
kminardo <kminardo@eb88d6b4-1ab2-431c-8348-e75b24139d5b>
2007-04-28 17:05:00 +0000

    Changed SpeedAlt Gun.

--- testing/data/stages/stage1.py
+++ testing/data/stages/stage1.py
@@ -1,6 +1,6 @@
 def start(p):
-	p.enemy_rows=3
-	p.enemy_cols=5
+	p.enemy_rows=7
+	p.enemy_cols=7
 	p.enemyodds=100
 	for player in p.playermanager:
-		player.change_gun("TestingGun","TestingBullet",p.playerbulletmanager,2)
+		player.change_gun("SwarmGun","Swarm",p.playerbulletmanager)
--- testing/gun.py
+++ testing/gun.py
@@ -74,12 +74,22 @@ class SpeedAlt(Gun):
 			a.set_pos(centerx,centery)
 			a.damage=self.damage
 			self.gunlist.add(a)
+class SwarmGun(Gun):
+	def shoot(self,rect):
+		centerx=rect.centerx
+		centery=rect.centery
+		for x in [-3,-2,0,2,3]:
+			a=self.bullet(self.gunlist)
+			a.bspeed=x
+			a.set_pos(centerx,centery)
+			a.damage=self.damage
+			self.gunlist.add(a)
 
 class TestingGun(Gun):
 	def shoot(self,rect):
 		centerx=rect.centerx
 		centery=rect.centery
-		for x in [2,2,5,9]:
+		for x in [-3,-2,0,2,3]:
 			a=self.bullet(self.gunlist)
 			a.bspeed=x
 			a.set_pos(centerx,centery)
